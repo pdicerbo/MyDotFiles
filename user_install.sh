cfe6125406459b3b040c72c2f884136438592832
Pierluigi Di Cerbo <pierluigi.dicerbo@gmail.com>
2025-10-02 16:27:34 +0200

    minor fixes to user_install script

--- user_install.sh
+++ user_install.sh
@@ -6,12 +6,14 @@ set -ex
 
 cd $HOME
 git clone --branch topic/hyprland --bare https://github.com/pdicerbo/dotfiles.git $HOME/.dotfiles-bare-repo
-git --git-dir=$HOME/.dotfiles-bare-repo --work-tree=$HOME checkout
+git --git-dir=$HOME/.dotfiles-bare-repo --work-tree=$HOME checkout -f
+
+export PATH=$PATH:$HOME/.scripts
 
 if [[ $# -eq 1 ]] ; then
-    cd $HOME
-    rm -rf MyDotFiles
-    exit 0  # the final user is expected to install the other packages
+    # this execution is for root user, so we can stop here;
+    # the final user is expected to install other packages
+    exit 0
 fi
 
 echo -e "\n\tinstalling Google Chrome...\n"
